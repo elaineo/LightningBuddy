7e1e99fa80990dc5119ebcee14b0e6da772f01e7
elaineo <elaine.ou@gmail.com>
2018-01-22 01:18:00 -0800

    run.py: fix tweet parser

--- run.py
+++ run.py
@@ -11,20 +11,20 @@ COMMANDS = ['CONNECT', 'FUNDCHANNEL', 'PAY', 'CLOSE']
 class LightningWrapper(LightningRpc):
     """API for Lightning RPC client
     """
-    def getinfo_(self):
+    def geturi(self):
         info = self.getinfo()
         node_id = info['id']
         node_address = info['address'][0]['address']
         node_port = info['port'] or 9735
-        "%s@%s:%d" % (node_id, node_address, node_port)
+        return "%s@%s:%d" % (node_id, node_address, node_port)
 
-    def connect(self, uri):
+    def _connect(self, uri):
         # need to validate this
         peer_id, peer_address = uri.split("@")
         peer_ip, peer_port = peer_address.split(":")
         self.connect(peer_id, peer_ip, peer_port)
 
-    def fundchannel(self, node_id, satoshis=cfg.CHANNEL_AMOUNT):
+    def _fundchannel(self, node_id, satoshis=cfg.CHANNEL_AMOUNT):
         self.fundchannel(node_id, satoshis)
 
 
@@ -35,18 +35,19 @@ def main():
     rpc_path = os.path.join(ln_path, 'lightning-rpc')
     logging.info(rpc_path)
 
-    ln = LightningWrapper(LightningRpc(rpc_path))
+    ln = LightningWrapper(rpc_path)
     bot = TwitterAPI(cfg.twitter['consumer_key'], cfg.twitter['consumer_secret'], 
         cfg.twitter['access_token'], cfg.twitter['access_token_secret'])
 
-    # Get info about Lightning node
-    info = ln.getinfo_()
-    logging.info(info)
+    # Get URI for Lightning node
+    uri = ln.geturi()
+    logging.info(uri)
 
     # Get info about twitter account
     account = bot.request('account/settings')
-    bot_name = account['screen_name']
-    owner = bot.request('users/lookup', {'screen_name': cfg.twitter_owner})
+    bot_name = account.get('screen_name')
+    owner_raw = bot.request('users/lookup', {'screen_name': cfg.twitter_owner})
+    owner = json.loads(owner_raw.response.text)
     owner_id = owner[0]['id_str']
 
     # Follow account owner, filter tweets based on bot's screen name
