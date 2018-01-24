f198181c14ff9f9fe11add9590fa051df49c0519
elaineo <elaine.ou@gmail.com>
2018-01-23 22:08:41 -0800

    tweet: Response framework

--- tweet.py
+++ tweet.py
@@ -6,16 +6,25 @@ HUMAN_COMMANDS = ['CONNECT', 'GIVE']
 BOT_COMMANDS = ['GETINFO', 'FUNDCHANNEL', 'PAY_INVOICE', 'INVOICE']
 
 class TweetClient:
-    def __init__(self, config, owner):
+    def __init__(self, config, db, owner):
         self.api = TwitterAPI(config['consumer_key'], config['consumer_secret'], 
             config['access_token'], config['access_token_secret'])
+        self.db = db
 
         bot_id = config['access_token'].split('-')[0]
         bot_raw = self.api.request('users/lookup', {'user_id': bot_id})
-        self.bot = json.loads(bot_raw.response.text)[0]
-
         owner_raw = self.api.request('users/lookup', {'screen_name': owner})
-        self.owner = json.loads(owner_raw.response.text)[0]
+
+        try:
+            self.bot = json.loads(bot_raw.response.text)[0]
+            self.owner = json.loads(owner_raw.response.text)[0]
+        except KeyError:
+            print("Cannot access Twitter account. Check API keys.")
+            raise 
+
+    def _post(self, msg, reply_sid):
+        tweet = self.api.request('statuses/update', {'status': msg, 'in_reply_to_status_id': reply_sid })
+        return tweet.get('id_str')
 
     def _filter(self, tweet):
         """ Filter for new instructions from human owner or other bots
@@ -42,11 +51,14 @@ class TweetClient:
         else:
             for c in BOT_COMMANDS:
                 if c in text:
-                    return c, user, tweet.get('user')
+                    return c, None, tweet.get('user')
 
         return None, None, None
 
-    def _response_status(self, tweet, command):
+    def _execute_response(self, sid, command, status):
+        logging.info(command)
+
+    def _resume_command(self, tweet, command):
         """ Continue executing an existing command
             if tweet.get('in_reply_to_status_id')
         """
@@ -61,10 +73,18 @@ class TweetClient:
             tweet = m.get('text')
             logging.info(m)
             if m.get('in_reply_to_status_id'):
-                response = self._response_status
+                response = self._resume_command(m)
+                # Look up command
+                # Respond to command
             else:
-                command, counterparty, commander = self._filter(m)
-                logging.info(command)
-                logging.info(counterparty)
-                logging.info(commander)
+                sid = m.get('id_str')
+                creator = m.get('user')
+                command, peer, bot = self._filter(m)
+                # Record new command
+                peer_uid = None if not peer else peer.get('id')
+                bot_uid = None if not bot else bot.get('id') 
+                self.db.commands.new(sid, command, creator.get('id'), peer_uid, bot_uid)
+                # Respond to command
+                self._execute_response(sid, command)
+            continue
        
