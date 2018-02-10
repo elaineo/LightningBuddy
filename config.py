19df817538a31c34681e21eb1f5c4e09771d3cfb
Elaine Ou <elaine.ou@gmail.com>
2018-02-09 21:43:25 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Twitter API keys (for LightningBuddy)
+twitter = { 'consumer_key': '',
+            'consumer_secret': '',
+            'access_token': '',
+            'access_token_secret': '' }
+
+# Username of LightningBuddy's owner
+twitter_owner = 'Elaine0u'
+
+## Default Channel creation parameters
+CHANNEL_AMOUNT = 100000  # in satoshis
+
+DB_PATH = 'sqlite.db'
