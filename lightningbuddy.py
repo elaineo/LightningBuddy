cedf6abf34598d8a92527b34527728b560402b1c
elaineo <elaine.ou@gmail.com>
2018-02-09 22:59:33 -0800

    lightningbuddy: fix errs

--- lightningbuddy.py
+++ lightningbuddy.py
@@ -18,35 +18,35 @@ class LightningWrapper(LightningRpc):
             node_address = info['address'][0]['address']
             node_port = info['port'] or 9735
             return "%s@%s:%d" % (node_id, node_address, node_port)
-        except ValueError, e:
+        except ValueError as e:
            logging.error(e)
 
     def get_invoice(self, amount, label, description=None):
         try:
             invoice = self.invoice(amount, "%s%s" % (label, str(random.randrange(999999))), description)
             return invoice['bolt11']
-        except ValueError, e:
+        except ValueError as e:
            logging.error(e)
 
     def _pay(self, bolt11):
         try:
             pay = self.pay(bolt11)
             return pay['preimage']
-        except ValueError, e:
+        except ValueError as e:
            logging.error(e)
 
     def _connect(self, node_id, host=None, port=None):
         try:
            connected = self.connect(peer_id, peer_ip, peer_port)
            return "Connected %s" + connected['id']
-        except ValueError, e:
+        except ValueError as e:
            logging.error(e)
 
     def _fundchannel(self, node_id, satoshis=cfg.CHANNEL_AMOUNT):
         try:
             tx = self.fundchannel(node_id, satoshis)
             return tx['funding_txid']
-        except ValueError, e:
+        except ValueError as e:
            logging.error(e)
         
 
@@ -54,7 +54,7 @@ class LightningWrapper(LightningRpc):
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    ln_path = os.getenv('LN_PATH') or os.path.join(os.getenv('HOME'), '.lightning')
+    ln_path = cfg.LN_PATH or os.path.join(os.getenv('HOME'), '.lightning')
     rpc_path = os.path.join(ln_path, 'lightning-rpc')
     logging.debug(rpc_path)
 
